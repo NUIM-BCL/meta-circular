6d0ee13adb0f27174843515b424379fed4e7fedb
Felix Filozov <ffilozov@gmail.com>
2014-10-24 15:50:58 +0100

    A metacircular evaluator called Camogie

---
+++ camogie.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+;;; Camogie supports the following:
+
+;; - elementary arithemtic
+;; - let expressions
+;; - if expressions
+;; - closures
+;; - booleans
+
+(define proto-env (list (cons '+ +) 
+                        (cons '- -)
+                        (cons '/ /)
+                        (cons '* *)
+                        (cons '< <)
+                        (cons '> >)
+                        (cons '<= <=)
+                        (cons '>= >=)))
+
+(define (eval expr env)
+  (match expr
+    [(? number?) expr]
+    [(? boolean?) expr]
+    [(? symbol?) (lookup env expr)]
+    [(list 'if condition true-branch false-branch) (if (eval condition env)
+                                                       (eval true-branch env)
+                                                       (eval false-branch env))]
+    [(list 'lambda params body) (list 'closure params body env)]
+    [(list 'let id '= val-expr 'in body) (eval body 
+                                               (extend-env env
+                                                           (list id)
+                                                           (list (eval val-expr env))))]
+    [(list f params ...) (let ((fp (eval f env))
+                               (args (map (lambda (a) (eval a env)) params)))
+                           (match fp
+                             [(list 'closure fparams fbody fenv)
+                                 (eval fbody (extend-env fenv
+                                                        fparams
+                                                        args))]
+                             [_ (apply fp args)]))]
+    [_ (error "eval: Failed to match expression: " expr)]))
+
+(define (lookup env id)
+  (match (assoc id proto-env)
+    [(cons v l) l]
+    [_ (match (assoc id env)
+         [(cons v l) l]
+         [_ (error "lookup: Failed to lookup identifier: " id)])]))
+
+(define (extend-env env ids vals)
+  (let ((bindings (map (lambda (id val) (binding id val)) ids vals)))
+    (append bindings env)))
+  
+(define (binding id val)
+  (cons id val))
+
