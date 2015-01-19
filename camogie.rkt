0c25516493e77625476399d7c442f16f808060dd
Felix Filozov <ffilozov@gmail.com>
2015-01-19 17:01:26 +0000

    Adding generic + and * operators

--- camogie.rkt
+++ camogie.rkt
@@ -1,7 +1,19 @@
 #lang racket
 
+(define proto-env '((num . 0)))
+
 (define (eval expr env)
   (match expr
+    [(list '+ a b) (l+ a b)]
+    [(list '* a b) (l* a b)]
+    [(list 'D f a) (eval `(tang ((lift ,f) (bundle ,a (num 1)))) env)]
+    [(list 'lift f) (let ([fp (eval f env)])
+                      (match fp
+                        [(list 'closure params body cenv)
+                         (list 'closure params body (lift-env cenv))]
+                        [_ (error "Coudl not lift" f)]))]
+    [(list 'num n) expr (let ([c (lookup env 'num)])
+                           (lift-num n c))]
     [(list 'bundle t1 t2) `(bundle ,(eval t1 env) ,(eval t2 env))]
     [(list 'tang t) (let ([tp (eval t env)])
                       (match tp
@@ -26,6 +38,27 @@
          [_ (error "eval: Cannot apply" fp)]))]
     [_ (error "eval: Failed to match expression" expr)]))
 
+(define (l+ a b)
+  (match* (a b)
+    [((? number?) (? number?)) (+ a b)]
+    [((list 'bundle a1 a2) (list 'bundle b1 b2)) 
+     `(bundle ,(l+ a1 b1) ,(l+ a2 b2))]
+    [((list 'num a1) (list 'num b1))
+     `(num ,(+ a1 b1))]
+    [(_ _) ("l+: Expecting bundle or num instead of" a b)]))
+
+(define (l* a b)
+  (match* (a b)
+    [((? number?) (? number?)) (* a b)]
+    [((list 'bundle a1 a2) (list 'bundle b1 b2))
+     `(bundle ,(l* a1 b1) ,(l+ (l* a1 b2) (l* a2 b1)))]
+    [((list 'num a1) (list 'num b1))
+     `(num ,(* a1 b1))]
+    [(_ _) ("l*: Expecting bundle or num instead of" a b)]))
+
+(define (lift-env env) #f)                  
+                              
+
 ;;; Lift number N, i.e. (num x), C times.
 ;;; Example: (lift-num 4 2)
 ;;;          => '(bundle (bundle (num 4) (num 0)) (bundle (num 0) (num 0)))
@@ -34,7 +67,7 @@
         [(> c 0) `(bundle ,(lift-num n (- c 1))
                           ,(lift-num 0 (- c 1)))]))
 
-;;; Lift numeric, i.e. (num x) or (bundle ...), as a constant.
+;;; Lift numeric N, i.e. (num x) or (bundle ...), as a constant.
 ;;; Example: (lift-numeric-as-const '(bundle (num 3) (num 1)))
 ;;;          => '(bundle (bundle (num 3) (num 1)) (bundle (num 0) (num 0)))
 (define (lift-numeric-as-const n)
@@ -44,8 +77,8 @@
 ;;; Replaces all (num x) with (num 0)
 ;;; Example: (zero-out-numeric '(bundle (bundle (num 3) (num 2)) (bundle (num 0) (num 4))))
 ;;;          => '(bundle (bundle (num 0) (num 0)) (bundle (num 0) (num 0)))
-(define (zero-out-numeric x)
-  (match x
+(define (zero-out-numeric n)
+  (match n
     [(list 'num _) `(num 0)]
     [(list 'bundle t1 t2) (list 'bundle (zero-out-numeric t1) 
                                         (zero-out-numeric t2))]))
